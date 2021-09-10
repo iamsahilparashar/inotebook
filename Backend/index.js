9656d55eb40b836db75e62b80e9c4434dc7ea0a6
iamsahilparashar <sahil_12013074@nitkkr.ac.in>
2021-09-10 05:39:18 +0530

    video 46

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./db');
+const express = require('express'); 
+
+const app = express();
+const port = 3000;
+connectToMongo();
+
+app.use(express.json());
+//Avlaible routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get('/',(req,res)=>{
+    res.send('Welcome');
+})
+
+app.listen(port,()=>{
+    console.log("server start listening on the port "+port);
+}); 
