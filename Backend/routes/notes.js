f8e367181e8acb7a2dc900049f3440a97b56dbe9
iamsahilparashar <sahil_12013074@nitkkr.ac.in>
2021-09-11 10:18:37 +0530

    updating notes

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -37,4 +37,33 @@ router.post('/addnotes', fetchuser, [
     }
 })
 
+//route 3 : update a existing notes
+router.put('/updatenote/:id', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description have more than 5 character').isLength({ min: 5 }),
+], async (req, res) => {
+    const {title, description, tag} = req.body;
+
+    //create a newNote obj;
+    const newNote = {};
+    if(title){newNote.title = title;}
+    if(description){newNote.description = description;}
+    if(tag){newNote.tag = tag;}
+
+    // find the Note to be updated and update it
+
+    let note = await Notes.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not found");
+    }
+    
+    if(note.user.toString() != req.user.id){
+        return res.status(401).send("Not allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote},{new : true});
+    res.json(note);
+
+})
+
 module.exports = router
