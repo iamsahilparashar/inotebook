fd9154320e4b94e9443c8af373b5d49090dee958
iamsahilparashar <sahil_12013074@nitkkr.ac.in>
2021-09-11 19:38:40 +0530

    useContext hook

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 
-function About() {
+const About = () => {
+    const a = useContext(noteContext)
+    useEffect(() => {
+        a.update();
+        // eslint-disable-next-line
+    }, [])
     return (
         <div>
-            this is about
+            This is About {a.state.name} and he is in class {a.state.class}
         </div>
     )
 }
